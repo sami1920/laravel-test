69ac991491753da8c82541dfc3de3f0a90c8e93d
Sami <samishafikue@gmail.com>
2024-01-10 02:02:29 +0500

    Hot fix

--- resources/views/thank-you.blade.php
+++ resources/views/thank-you.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="#">Test App</a>
+            <a class="navbar-brand" href="{{ route('home') }}">Test App</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
